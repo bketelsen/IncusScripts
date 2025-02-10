52ae3a57922635dc543dad844d39a8d73b21ed06
Brian Ketelsen <bketelsen@gmail.com>
2025-02-10 13:33:10 -0500

    feat: archlinux-vm-install

---
+++ install/archlinux-vm-install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2025 tteck
+# Author: tteck (tteckster)
+# License: MIT
+# https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+
+
+msg_info "Cleaning up"
+
+msg_ok "Cleaned"
+# Modified by surgeon https://github.com/bketelsen/surgeon
